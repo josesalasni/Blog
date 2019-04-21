33bae9906ada4736e38ba8b562db172715af482f
Carlos José Salas <josesalasni@gmail.com>
2019-04-21 17:10:44 -0500

    testing comments fb

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -6,80 +6,94 @@ import Bio from '../components/Bio'
 import MainLayout from '../components/Layout'
 import SEO from '../components/seo'
 import { rhythm, scale } from '../utils/typography'
+import { async } from 'q';
 
 
 class BlogPostTemplate extends React.Component {
-  render() {
-    const post = this.props.data.markdownRemark
-    const siteTitle = this.props.data.site.siteMetadata.title
-    const { previous, next } = this.props.pageContext
 
-    return (
-      <MainLayout location={this.props.location} title={siteTitle}>
-        <SEO title={post.frontmatter.title} description={post.excerpt} />
-        <h1 style={{marginTop: '0'}}>{post.frontmatter.title}</h1>
-        <p
-          style={{
-            ...scale(-1 / 5),
-            display: `block`,
-            marginBottom: rhythm(1),
-            marginTop: rhythm(-1),
-          }}
-        >
-        <Icon style={{paddingTop: '15px', paddingRight: '15px'}} type="calendar" />
-          {post.frontmatter.date}
-        </p>
-        <div dangerouslySetInnerHTML={{ __html: post.html }} />
-        <hr
-          style={{
-            marginBottom: rhythm(1),
-          }}
-        />
+    componentDidMount(){
 
-        {post.frontmatter.tags.map((tag, index) => {
+        const script = document.createElement("script");
+
+        script.src = "https://connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v3.2&appId=312715572758197&autoLogAppEvents=1";
+        script.async = true;
+        script.crossOrigin="anonymous";
+
+        document.body.appendChild(script);
+    }
+
+    render() {
+        const post = this.props.data.markdownRemark
+        const siteTitle = this.props.data.site.siteMetadata.title
+        const { previous, next } = this.props.pageContext
+
         return (
-            <span key={index} className="tag">
-                <Tag >
-                    <Link style={{boxShadow: "none" }}  to={`/tags/${tag}`}>{tag}</Link>
-                </Tag>
-                
-            </span>
-          )
-        }
-        )}
+        <MainLayout location={this.props.location} title={siteTitle}>
+            <SEO title={post.frontmatter.title} description={post.excerpt} />
+            <div id="fb-root"></div>
+            <h1 style={{marginTop: '0'}}>{post.frontmatter.title}</h1>
+            <p
+            style={{
+                ...scale(-1 / 5),
+                display: `block`,
+                marginBottom: rhythm(1),
+                marginTop: rhythm(-1),
+            }}
+            >
+            <Icon style={{paddingTop: '15px', paddingRight: '15px'}} type="calendar" />
+            {post.frontmatter.date}
+            </p>
+            <div dangerouslySetInnerHTML={{ __html: post.html }} />
+            <hr
+            style={{
+                marginBottom: rhythm(1),
+            }}
+            />
 
-
-        <Bio />
-
-        <ul
-          style={{
-            display: `flex`,
-            flexWrap: `wrap`,
-            justifyContent: `space-between`,
-            listStyle: `none`,
-            marginTop: '30px',
-            padding: 0,
-          }}
-        >
-          <li>
-            {previous && (
-              <Link to={previous.fields.slug} rel="prev">
-                ← {previous.frontmatter.title}
-              </Link>
+            {post.frontmatter.tags.map((tag, index) => {
+            return (
+                <span key={index} className="tag">
+                    <Tag >
+                        <Link style={{boxShadow: "none" }}  to={`/tags/${tag}`}>{tag}</Link>
+                    </Tag>
+                    
+                </span>
+                )}
             )}
-          </li>
-          <li>
-            {next && (
-              <Link to={next.fields.slug} rel="next">
-                {next.frontmatter.title} →
-              </Link>
-            )}
-          </li>
-        </ul>
 
-      </MainLayout>
-    )
-  }
+            
+            <Bio />
+
+            <ul
+            style={{
+                display: `flex`,
+                flexWrap: `wrap`,
+                justifyContent: `space-between`,
+                listStyle: `none`,
+                marginTop: '30px',
+                padding: 0,
+            }}
+            >
+            <li>
+                {previous && (
+                <Link to={previous.fields.slug} rel="prev">
+                    ← {previous.frontmatter.title}
+                </Link>
+                )}
+            </li>
+            <li>
+                {next && (
+                <Link to={next.fields.slug} rel="next">
+                    {next.frontmatter.title} →
+                </Link>
+                )}
+            </li>
+            </ul>
+
+            <div class="fb-comments" data-href={window.location.href} data-numposts="6"></div>
+        </MainLayout>
+        )
+    }
 }
 
 export default BlogPostTemplate
